ffc7cf41e70bde3e9f9bcc7d5e595c5a34a934ea
Bharath M <gowdambharath718@gmail.com>
2026-03-22 11:31:47 +0530

    lowerbound and upperbound

---
+++ arrays/Upperbound.java
@@ -0,0 +1,43 @@
+package DSA;
+
+public class Upperbound {
+
+
+    static int   upperBound(int [] arr , int x)
+
+    {
+
+
+        if ( x>arr[arr.length-1])
+        {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length-1;
+        while( low<=high)
+        {
+            int mid = low + ( high-low)/2;
+
+            if (arr[mid ]>x)
+                high = mid-1;
+            else
+            {
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int [] arr = { 1,2,2,2,3,4,5};
+        int result = upperBound(arr,1);
+        if ( result != -1)
+        {
+            System.out.println(" the upperBound  is " + result);
+
+        }
+        else
+        {
+            System.out.println("  Given target is Out of range ");
+        }
+    }
+}
