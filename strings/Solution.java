0542edb51eab2be8241dcd91dfcb2ee6237c6c53
Bharath M <gowdambharath718@gmail.com>
2026-05-14 22:28:48 +0530

    string

---
+++ strings/Solution.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.HashSet;
+
+public class Solution {
+
+        public int lengthOfLongestSubstring(String s) {
+
+            HashSet<Character> set = new HashSet<>();
+
+            int left = 0;
+            int max = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+
+                while (set.contains(s.charAt(right))) {
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+
+                set.add(s.charAt(right));
+
+                max = Math.max(max, right - left + 1);
+            }
+
+            return max;
+        }
+    }
+
