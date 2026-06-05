7adf2bc06646e1f1c6e285f3e76ab83e4b4a506b
Bharath M <gowdambharath718@gmail.com>
2026-06-05 22:34:35 +0530

    dll

---
+++ linked_list/InsertOperations.java
@@ -0,0 +1,56 @@
+package DSA.DoubleLL;
+
+public class InsertOperations {
+
+private static Node head ;
+private static  Node tail;
+
+
+    static  Node insertAtbegin( int data )
+    {
+        Node newNode = new Node(data);
+
+// if list is empty
+        if( head == null )
+        {
+
+            head.next = newNode;
+            tail= newNode ;
+            return head ;
+        }
+// already list has some elements
+        newNode.next = head ;
+        head.prev = newNode;
+        head = newNode;
+return head ;
+
+    }
+
+
+    static  void traverse ()
+    {
+        Node temp;
+        if( head == null)
+        {
+            System.out.println(" empty list ");
+        }
+        else
+        {
+
+            temp = head ;
+            while( temp.next  != null)
+            {
+
+                temp = temp.next ;
+            }
+
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
