c6a9cddb17912cc4e00dc18319046f693063814d
Bharath M <gowdambharath718@gmail.com>
2026-09-11 22:30:55 +0530

    arrays

---
+++ arrays/linearSearch.java
@@ -0,0 +1,23 @@
+package DSA.BasicArrays;
+
+class linearSearch{
+    public int search(int arr[], int x) {
+
+
+
+
+
+        for( int i =0 ; i<arr.length;i++)
+        {
+
+            if( arr[i] == x)
+            {
+                return i ;
+            }
+
+
+        }
+        return -1;
+
+    }
+}
